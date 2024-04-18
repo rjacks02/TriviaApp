961f938c1abf12713d15dee2c4aa88375c25e176
thebugslayerman <mmacek1@villanova.edu>
2024-04-18 11:07:46 -0400

    re-added style sheet

---
+++ styles.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#fff',
+    },
+    title: {
+      fontSize: 28,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    questionContainer: {
+      marginBottom: 20,
+    },
+    questionTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    questionText: {
+      fontSize: 16,
+      color: '#333',
+    },
+    answerContainer: {
+      marginBottom: 20,
+    },
+    answerTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    answerText: {
+      fontSize: 16,
+      color: '#333',
+    },
+  });
